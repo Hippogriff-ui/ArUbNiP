d7153dd3a59a8e0ec40b582eae87af527cd38ab5
Hippogriff-ui <82009382+Hippogriff-ui@users.noreply.github.com>
2024-06-26 21:26:37 +0800

    Update PSSM_extraction_script.m

--- code/feature_extraction/PSSM/PSSM_extraction_script.m
+++ code/feature_extraction/PSSM/PSSM_extraction_script.m
@@ -29,7 +29,7 @@ for i = 1:length(file_list)
         rl = 14;
         mp = ll + 1;
         feat=pssm2feat(pssm_matrix,0,0,ll,mp,rl);
-        Save_Path = strcat('C:\Users\Li Xiaokang\Desktop\pssm_31_mulspecies\','PSSM_',filename);
+        Save_Path = strcat('D:\paper\ubiquitination\Arab\feature_extraction\','PSSM_',filename);
         Fun_Save_txt(Save_Path, feat);
     end
 end
